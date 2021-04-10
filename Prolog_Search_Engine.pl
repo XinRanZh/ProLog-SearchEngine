5ee582c5cc4e2860d328d6cfd06c5218f83a0fbd
XinRanZh <799177630@qq.com>
2021-04-10 23:18:30 +0800

    Now allow to find the content of the article; When searching for the content of the article, the string in same order prefered
    Now, when searching for keywords, the ones in the same order are preferred

--- Prolog_Search_Engine.pl
+++ Prolog_Search_Engine.pl
@@ -1,7 +1,31 @@
-article(("Hello", ["asdf", "jkl"], ["wtf", "123"])).
+article(("Hello", "love sweet word", "I Love Julia Liao")).
+article(("meow", "word sweet", "Nice Job, Well Done")).
 
-fitTitle(Key, (Title, Keywords, Element)):- Key = Title.
 
-search(Key, Article):- article(Article), fitTitle(Key, Article);article(Article), fitKeywords(Key, Article).
 
-fitKeywords(Key, (Title, Keywords, Element)):- member(Key, Keywords).
+
+fitTitle(Key, (Title, Keywords, Content)):- Key = Title.
+
+fitKeywordsDirect(Key, (Title, Keywords, Content)):- isSubString(Key, Keywords).
+
+fitKeywordsSplit(Key, (Title, Keywords, Content)):- checkSplit(Key, Keywords).
+
+fitContentDirect(Key, (Title, Keywords, Content)):- isSubString(Key, Content).
+
+fitContentSplit(Key, (Title, Keywords, Content)):- checkSplit(Key, Content).
+
+search(Key, Articles):- distinct(search_mayDuplicate(Key, Articles)).
+
+search_mayDuplicate(Key, Articles):- 
+                        article(Articles), fitTitle(Key, Articles); 
+                        article(Articles), fitKeywordsDirect(Key, Articles);
+                        article(Articles), fitKeywordsSplit(Key, Articles); 
+                        article(Articles), fitContentDirect(Key, Articles);
+                        article(Articles), fitContentSplit(Key, Articles).
+
+
+checkSplit(Keywords, String):- split_string(Keywords, " ", ".", KeyList), checkSplitList(KeyList, String).
+
+checkSplitList([H|T], String):- isSubString(H, String); T \= [], checkSplitList(T, String).
+
+isSubString(SubString, String):- string_codes(SubString, X), string_codes(String, Y), append(_, Any, Y), append(X, _, Any), X \= [].
