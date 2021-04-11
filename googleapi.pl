93836ee705c1baed5bfc93ecd48117d0169ada74
XinRanZh <799177630@qq.com>
2021-04-11 10:12:32 +0800

    Fixed a bug that cause issue in search parameter include a space

--- googleapi.pl
+++ googleapi.pl
@@ -5,7 +5,8 @@
 google_url("https://customsearch.googleapis.com/customsearch/v1?key=AIzaSyBerH2Ssk2Z6Cn7savDLYGwb4dYBM_1c6A&cx=8dbbef1031a520106&q=").
 
 search(Keywords, Response):- google_url(X), 
-                             string_concat(X, Keywords, Url), 
+                             re_replace(" ", "+", Keywords, ValidKeyWord),
+                             string_concat(X, ValidKeyWord, Url), 
                              http_get(Url, Json, []), 
                              atom_json_dict(Json, Dict, []), 
                              [Head|Tail] = Dict.items, 
