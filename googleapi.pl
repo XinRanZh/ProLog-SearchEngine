6b20092c375ed96ef225054423ba616b3b3ff503
XinRanZh <799177630@qq.com>
2021-04-11 10:25:34 +0800

    Comment added

--- googleapi.pl
+++ googleapi.pl
@@ -2,12 +2,21 @@
 :- use_module(library(http/json)).
 :- use_module(library(uri)).
 
+
+% A url that Contain Correct Api-Key and Search Engine Key
 google_url("https://customsearch.googleapis.com/customsearch/v1?key=AIzaSyBerH2Ssk2Z6Cn7savDLYGwb4dYBM_1c6A&cx=8dbbef1031a520106&q=").
 
+% Search Via Google Custon Search Engine
 search(Keywords, Response):- google_url(X), 
+                             % First Convert every SPACE to +
                              re_replace(" ", "+", Keywords, ValidKeyWord),
+                             % Link a valid keyword to URL with API
                              string_concat(X, ValidKeyWord, Url), 
+                             % Chase a simple HTTP Get request
                              http_get(Url, Json, []), 
+                             % Convert the JSON result to a Dict
                              atom_json_dict(Json, Dict, []), 
+                             % I only want the first result, which is the first one in the JSON.item
                              [Head|Tail] = Dict.items, 
+                             % Then The Respose is it's Snippet 
                              Response = Head.htmlSnippet.
