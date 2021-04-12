96fc07a2a7bd1e3e0b52959131dcf6c7ecb6960f
Andrew <andrewlin20@gmail.com>
2021-04-12 16:06:48 +0800

    added cui

---
+++ cui.pl
@@ -0,0 +1,22 @@
+:- include('googleapi.pl').
+:- include('Prolog_Search_Engine.pl').
+:- dynamic(article/1).
+start() :-
+    write('Type search to use the search engine, or add to add an article to the knowledge base'),
+    nl,
+    read(Choice),
+    (
+        Choice == 'search' ->
+        write('Type in your search query'),
+        read(Input),
+        search(Input, Response),
+        write(Response)
+        ;
+        Choice == 'add' ->
+        write('Type in the name of the article you wish to add'),
+        read(Name),
+        add_article(Name)
+
+
+    ).
+
